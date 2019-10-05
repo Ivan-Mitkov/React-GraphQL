ea0c7210222fb2607ee4b4d67f6b3b3c9c2ee7bc
unknown <ivan.mitkov@gmail.com>
2019-10-05 18:11:23 +0300

    yoga config

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({path:'variables.env'});
+const CreateServer=require('./createServer');
+const db=require('./db');
+
+const server=CreateServer()
+
+//TODO use express middleware to handle cookies (JWT)
+//TODO use express middleware to populate current user
+
+server.start({
+    cors:{
+        credntials:true,
+        origin:process.env.FRONTEND_URL,
+    },
+
+},deets=>{
+    console.log(`Server is running on http://localhost/${deets.port}`)
+}
+
+)
